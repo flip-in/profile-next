9aa0f700250693f003ef80078c41d8a30196242e
Will Price <pricew86@gmail.com>
2023-07-04 13:49:04 +0800

    change deploy workflow

--- next.config.js
+++ next.config.js
@@ -1,22 +1,14 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
 
-const isGithubActions = process.env.GITHUB_ACTIONS || false;
-
-let assetPrefix = "";
-let basePath = "/";
-
-if (isGithubActions) {
-  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, "");
-
-  assetPrefix = `/${repo}/`;
-  basePath = `/${repo}`;
-}
-
-module.exports = {
-  assetPrefix: assetPrefix,
-  basePath: basePath,
-  images: {
-    loader: "imgix",
-    path: "kinexus-3274.imgix.net",
+/**
+ * @type {import('next').NextConfig}
+ **/
+const nextConfig = {
+  output: "export",
+  basePath: "/gh-pages-test",
+  experimental: {
+    appDir: true,
   },
 };
+
+module.exports = nextConfig;
