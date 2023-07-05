30c0d5b460e5d5f621f4d291f01cf0372598d8ff
Will Price <pricew86@gmail.com>
2023-07-05 14:48:49 +0800

    add sections

--- app/(shared)/Navbar.tsx
+++ app/(shared)/Navbar.tsx
@@ -11,12 +11,12 @@ const Navbar = (props: Props) => {
     <header>
       <nav className="flex py-5 items-center justify-center ">
         <div className="flex items-center gap-7 lg:text-lg md:text-md max-sm:text-sm flex-grow justify-evenly mx-5">
-          <a href="#about">About</a>
-          <a href="#portfolio">Portfolio</a>
+          <a href="#About">About</a>
+          <a href="#Portfolio">Portfolio</a>
           <div className={styles.navbarLogo}>
             <Image alt="William Price logo" placeholder="blur" src={navLogo} />
           </div>
-          <a href="#contact">Contact</a>
+          <a href="#Contact">Contact</a>
           <a href="#home">Home</a>
         </div>
       </nav>
--- app/page.tsx
+++ app/page.tsx
@@ -7,14 +7,44 @@ type Props = {};
 
 function Home({}: Props) {
   return (
-    <div
-      className="flex items-start justify-center bg-center bg-no-repeat h-[500px]"
-      style={{
-        backgroundImage: `url(${bg.src})`,
-        backgroundSize: "500px",
-      }}
-    >
-      <Image src={taglineImg} height={200} alt="name logo" />
+    <div>
+      <div
+        id="hero-background"
+        className="flex items-start justify-center bg-center bg-no-repeat h-[500px]"
+        style={{
+          backgroundImage: `url(${bg.src})`,
+          backgroundSize: "500px",
+        }}
+      >
+        <Image id="hero-logo" src={taglineImg} height={200} alt="name logo" />
+      </div>
+
+      <div>
+        <section id="About" className="bg-gray-200 py-8">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-4">About</h2>
+            <p>This is the About section.</p>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <section id="Portfolio" className="bg-white py-8">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-4">Portfolio</h2>
+            <p>This is the Portfolio section.</p>
+          </div>
+        </section>
+      </div>
+
+      <div>
+        <section id="Contact" className="bg-gray-200 py-8">
+          <div className="container mx-auto px-4">
+            <h2 className="text-3xl font-bold mb-4">Contact</h2>
+            <p>This is the Contact section.</p>
+          </div>
+        </section>
+      </div>
     </div>
   );
 }
