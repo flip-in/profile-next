2379420c4d1956d475ec371981fbbc563e8a04f7
Will Price <pricew86@gmail.com>
2023-07-12 17:01:52 +0800

    update font and start about section

--- app/(home)/About.tsx
+++ app/(home)/About.tsx
@@ -1,17 +1,42 @@
 import React from "react";
+import Card from "../(shared)/Card";
 
 interface AboutProps {}
 
 const About: React.FC<AboutProps> = () => {
   return (
-    <div className="my-5">
-      <section id="About" className="bg-gray-200 py-8">
-        <div className="flex flex-col mx-auto px-4 container text-center">
-          <h2 className="text-3xl font-bold mb-4">About</h2>
-          <p>This is the About section.</p>
+    <section id="About" className="py-8">
+      {/* INTRO */}
+      <div className="sm:flex ">
+        <div className="basis-1/2">
+          <h1 className="text-4xl py-4">Hi, I&apos;m William Price</h1>
+          <p className="text-lg">
+            Software Developer and Manager, <br></br> Engineering and Product
+            Development
+          </p>
+          <p>
+            I build processes for the engineering team to grow and deliver
+            high-quality products. I have over three years of experience in a
+            technical leadership role, and I have a Full-Stack Web Developer
+            certification from Le Wagon and two Javascript certifications from
+            ui.dev.
+          </p>
         </div>
-      </section>
-    </div>
+        <div className="sm:flex basis-1/2 flex-col gap-2">
+          <div className="">
+            <p>Highlights</p>
+          </div>
+          <div className=" mb-2">
+            <Card />
+          </div>
+          <div className=" mb-2">
+            <Card />
+          </div>
+        </div>
+      </div>
+
+      {/* TECHNOLOGIES */}
+    </section>
   );
 };
 
---
+++ app/(shared)/Card.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+type Props = {};
+
+const Card = (props: Props) => {
+  return (
+    <>
+      <div>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
+        et fugit asperiores! Ut officia quam ad ratione odio nobis sit
+        consequatur id dolore? Libero animi in consectetur exercitationem sed
+        est?
+      </div>
+    </>
+  );
+};
+
+export default Card;
