37607291e7816bddc82376cca69167429fc22e4f
Will Price <pricew86@gmail.com>
2023-07-12 19:44:54 +0800

    Add content for about section

--- app/(shared)/Card.tsx
+++ app/(shared)/Card.tsx
@@ -1,15 +1,29 @@
 import React from "react";
 
-type Props = {};
+type Props = {
+  jobTitle?: string;
+  description?: string;
+  link?: string;
+};
 
-const Card = (props: Props) => {
+const Card = ({ jobTitle, description, link }: Props) => {
   return (
     <>
-      <div>
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus
-        et fugit asperiores! Ut officia quam ad ratione odio nobis sit
-        consequatur id dolore? Libero animi in consectetur exercitationem sed
-        est?
+      <div className="bg-white p-4 sm:mt-0 mt-5">
+        <div>
+          <span className="text-sm text-wh-300">{jobTitle}</span>
+        </div>
+        <div>
+          <p className="mt-1">{description}</p>
+        </div>
+        {/* TODO: Add some styling to the link */}
+        <div className="mt-5 text-sm">
+          {link && (
+            <a target="_blank" href={link}>
+              Work Experience
+            </a>
+          )}
+        </div>
       </div>
     </>
   );
