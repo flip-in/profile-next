d93a0216b9597a76a1d2e172a052b09a5da71233
Will Price <pricew86@gmail.com>
2023-07-16 19:35:10 +0800

    Contact form styling

--- app/(home)/Contact.tsx
+++ app/(home)/Contact.tsx
@@ -1,17 +1,86 @@
-import React from "react";
+"use client";
+import React, { ChangeEvent, FormEvent, useState } from "react";
 
-interface ContactProps {}
+interface FormState {
+  name: string;
+  email: string;
+  message: string;
+}
 
-const Contact: React.FC<ContactProps> = () => {
+const Contact: React.FC = () => {
+  const [formState, setFormState] = useState<FormState>({
+    name: "",
+    email: "",
+    message: "",
+  });
+
+  const handleSubmit = (event: FormEvent) => {
+    event.preventDefault();
+    console.log(formState);
+  };
+
+  const handleInput = (
+    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setFormState({
+      ...formState,
+      [event.target.name]: event.target.value,
+    });
+  };
+
   return (
-    <div className="my-5">
-      <section id="Contact" className="bg-gray-200 py-8">
-        <div className="container mx-auto px-4 flex flex-col text-center">
-          <h2 className="text-3xl font-bold mb-4">Contact</h2>
-          <p>This is the Contact section.</p>
+    <section id="Contact">
+      <main className="relative py-28 sm:py-20 bg-gray-900 sm:bg-inherit">
+        <div className="relative z-10 max-w-screen-xl mx-auto text-gray-600 sm:px-4 md:px-8">
+          <div className="max-w-lg space-y-3 px-4 sm:mx-auto sm:text-center sm:px-0">
+            <h3 className="text-bl-900 font-semibold">Contact</h3>
+            <p className="text-white sm:text-black text-3xl font-semibold sm:text-4xl">
+              Get in touch
+            </p>
+            <p className="text-gray-300 sm:text-gray-600">
+              I&apos;d love to hear from you! Please fill out the form bellow.
+            </p>
+          </div>
+          <div className="mt-12 mx-auto px-4 p-8 bg-white sm:max-w-lg sm:px-8 sm:rounded-xl">
+            <form onSubmit={(e) => e.preventDefault()} className="space-y-5">
+              <div>
+                <label className="font-medium">Full name</label>
+                <input
+                  type="text"
+                  required
+                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                />
+              </div>
+              <div>
+                <label className="font-medium">Email</label>
+                <input
+                  type="email"
+                  required
+                  className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                />
+              </div>
+              <div>
+                <label className="font-medium">Message</label>
+                <textarea
+                  required
+                  className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-gray-800 shadow-sm rounded-lg"
+                ></textarea>
+              </div>
+              <button className="w-full px-4 py-2 text-white font-medium bg-bl-900 hover:bg-gray-400 active:bg-gray-900 rounded-lg duration-150">
+                Submit
+              </button>
+            </form>
+          </div>
         </div>
-      </section>
-    </div>
+        <div
+          className="visible sm:hidden absolute inset-0 blur-[118px] max-w-lg h-[800px] mx-auto sm:max-w-3xl sm:h-[400px]"
+          style={{
+            background:
+              "linear-gradient(106.89deg, rgba(192, 132, 252, 0.11) 15.73%, rgba(14, 165, 233, 0.41) 15.74%, rgba(232, 121, 249, 0.26) 56.49%, rgba(79, 70, 229, 0.4) 115.91%)",
+          }}
+        ></div>
+      </main>
+    </section>
   );
 };
 
